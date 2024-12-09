1ee355d97ea122b965f65802df27ad1f3e706b7c
PrazolMalla <Prajwalmalla16@gmail.com>
2024-12-09 11:51:16 +0545

    hamburger menu scroll disable

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,15 +1,18 @@
 const hamMenu = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".mobile-nav-bar");
 const cross = document.querySelector(".nav-cross");
+const body = document.querySelector(".body");
 
 hamMenu.addEventListener("click", () => {
   navMenu.classList.add("show");
   cross.classList.add("show");
   hamMenu.classList.add("hide");
+  body.classList.add("overflow");
 });
 
 cross.addEventListener("click", () => {
   navMenu.classList.remove("show");
   cross.classList.remove("show");
   hamMenu.classList.remove("hide");
+  body.classList.remove("overflow");
 });
